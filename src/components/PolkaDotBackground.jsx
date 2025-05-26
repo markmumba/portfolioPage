224a13fb78bd771c17718fef10f9d573be6ddd4a
markian mwangi <50865281+markmumba@users.noreply.github.com>
2025-05-26 14:49:28 +0300

    moving the polka dots to the back

--- src/components/PolkaDotBackground.jsx
+++ src/components/PolkaDotBackground.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const PolkaDotBackground = () => {
   return (
-    <div className="absolute inset-0">
+    <div className="absolute inset-0 -z-10">
       <div className="absolute inset-0 bg-[radial-gradient(#ffffff_1px,transparent_1px)] [background-size:16px_16px] opacity-30"></div>
     </div>
   );
